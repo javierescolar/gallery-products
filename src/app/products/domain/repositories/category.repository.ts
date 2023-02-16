b01b967eda07b53eccd25f9b2e89caf6f1e5bde9
Javier <javierescolar10@gmail.com>
2023-02-16 16:27:00 +0100

    feat: :construction: create entities and repositories

---
+++ src/app/products/domain/repositories/category.repository.ts
@@ -0,0 +1,7 @@
+import { Observable } from 'rxjs';
+import { Category } from '../entities/category';
+
+export abstract class CategoryRepository {
+  abstract getAll(): Observable<Category[]>;
+  abstract getOne(id: number): Observable<Category>;
+}
---
+++ src/app/products/domain/repositories/product.repository.ts
@@ -0,0 +1,7 @@
+import { Observable } from 'rxjs';
+import { Product } from '../entities/product';
+
+export abstract class ProductRepository {
+  abstract getAll(): Observable<Product[]>;
+  abstract getOne(id: number): Observable<Product>;
+}
