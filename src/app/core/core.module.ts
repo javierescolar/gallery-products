64e1ee2c719cb7941b203fe286ed21a322964dc8
Javier <javierescolar10@gmail.com>
2023-03-01 16:29:42 +0100

    feat: :sparkles: footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,16 +5,23 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
 import { MenuNavComponent } from './shared/menu-nav/menu-nav.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [NavbarComponent, MenuNavComponent, SpinnerComponent],
+  declarations: [
+    NavbarComponent,
+    MenuNavComponent,
+    SpinnerComponent,
+    FooterComponent,
+  ],
   exports: [
     NavbarComponent,
     MenuNavComponent,
     HttpClientModule,
     SpinnerComponent,
     RouterModule,
+    FooterComponent,
   ],
 })
 export class CoreModule {}
